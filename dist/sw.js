3d2a16bd73c70fc526e434c15ce35efff65ef6bd
Dominic Gonzales <beans@Dominics-MacBook-Air.local>
2026-09-22 08:02:07 +0800

    Redesign product shell with editorial styling

--- dist/sw.js
+++ dist/sw.js
@@ -1,4 +1,4 @@
-const CACHE='afterhours-fitness-shell-20260922-methods-v7';
+const CACHE='afterhours-fitness-shell-20260922-editorial-v8';
 const ASSETS=['/','/index.html','/style.css','/app.js','/core.js','/content.js','/nutrition-data.js','/manifest.webmanifest','/icon.svg','/icon-192.png','/icon-512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('afterhours-fitness-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
